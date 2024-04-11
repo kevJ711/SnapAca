212b1712a7a0debd779d984d71c7cdf1f33c0a95
kevJ711 <76807356+kevJ711@users.noreply.github.com>
2024-04-11 01:06:59 -0700

    Delete welcome.js

--- welcome.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("enterButton").addEventListener("click", function() {
-    window.location.href = "index.html"; // Replace "music_catalog.html" with the filename of your music catalog page
-});
