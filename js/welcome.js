f4c7f15ea6024013b41295ffd0178b3c7a8f035c
kevJ711 <76807356+kevJ711@users.noreply.github.com>
2024-04-22 21:36:23 -0700

    Update welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -1,3 +1,3 @@
 document.getElementById("enterButton").addEventListener("click", function() {
-    window.location.href = "index.html"; // Replace "music_catalog.html" with the filename of your music catalog page
+    window.location.href = "index.html"; 
 });
